9bedf9f3480b68dc70bdf8d36b7cedaec59aafea
MUSA YILMAZ <yilmazmusa66@gmail.com>
2023-11-21 03:26:01 +0300

    Client tarafında gelen istekleri karşılamak için ViewModeller oluşturuldu.

--- Order.API/Models/Order.cs
+++ Order.API/Models/Order.cs
@@ -5,11 +5,11 @@ namespace Order.API.Models
     public class Order
     {
         public Guid Id { get; set; }
-        public string BuyerId { get; set; }
+        public Guid BuyerId { get; set; } // Bu BuyerId yi CreateOrderVM tarafında string ile alıyoruz ihtiyaç olursa  içerde Guid'e çaviriyoruz.
+
+        public string? Description { get; set; }
             
-        public string Description { get; set; }
-            
-        public List<OrderItem> OrderItems { get; set; }
+        public List<OrderItem>? OrderItems { get; set; }  //Bir Order'ın birdern fazla özelliği olur onları da OrderItem'da tutarız.
 
         public OrderStatus  OrderStatus{ get; set; }
         public DateTime CreatedDate { get; set; }
